230bc6d1e299e3c723086f9eacd256e375ec9af0
Mohamed-Hassan-M-M <mohamedhassan@cis.asu.edu.eg>
2023-03-18 16:11:04 +0200

    change h barcode

--- my-assets/js/admin_js/json/customer_add_refund.js.php
+++ my-assets/js/admin_js/json/customer_add_refund.js.php
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 <?php
 $cache_file = "customer.json";
    header('Content-Type: text/javascript; charset=utf8');
@@ -6,7 +6,7 @@ $cache_file = "customer.json";
    header("Cache-Control: post-check=0, pre-check=0", false);
    header("Pragma: no-cache");
 ?>
-var customerList = <?php echo file_get_contents($cache_file); ?> ;
+//var customerList = <?php //echo file_get_contents($cache_file); ?>// ;
 var csrf_test_name=  $("#CSRF_TOKEN").val();
 
 var APchange = function(event, ui){
@@ -16,8 +16,8 @@ $(function() {
   
     $( ".customerSelection" ).autocomplete(
 	{
-        source:customerList,
-        /*source:function (request, response) {
+        //source:customerList,
+        source:function (request, response) {
             var customer_name = $("#customer_name").val();
             $.ajax({
                 url: base_url + "dashboard/Cinvoice/customer_search_all_customers",
@@ -31,7 +31,7 @@ $(function() {
                     response(jQuery.parseJSON(data));
                 },
             });
-        },*/
+        },
 		delay:300,
 		focus: function(event, ui) {
 			$(this).parent().find(".customer_hidden_value").val(ui.item.value);
